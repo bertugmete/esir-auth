2b5fc8e2390238c883c7a61f5e7f8cb64c466f0e
Bertuğ Mete <bertumete95@gmail.com>
2022-09-22 15:17:13 +0300

    init

---
+++ src/test/java/tr/com/mkk/esir/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.mkk.esir.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
